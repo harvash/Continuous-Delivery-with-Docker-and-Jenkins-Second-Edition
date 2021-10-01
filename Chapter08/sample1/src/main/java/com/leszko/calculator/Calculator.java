3d66a2098ac4fcb7ebfd8bcff5177a724b323b25
CXA791 <elevenbravo.ret@gmail.com>
2021-10-01 00:35:25 -0400

    update umlNUMBER1 to NUMBER1

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /** Calculator main */
 @Service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
+        final static int NUMBER1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;

cba51a323c2f041558f7a1ac60a107f962c0c24f
CXA791 <elevenbravo.ret@gmail.com>
2021-10-01 00:09:07 -0400

    and javadoc comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/** Calculator main */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
